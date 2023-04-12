ab3f634db4dba0cfad3e3eeec030c5d3c543e7fe
Rush Perera <104535750+Rush-Perera@users.noreply.github.com>
2023-04-12 16:09:22 +0530

    IOS like ui test

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,72 @@
-// ignore_for_file: prefer_const_constructors
+import 'package:flutter/cupertino.dart';
 
-import 'package:flutter/material.dart';
+void main() => runApp(const ActionSheetApp());
 
-main() {
-  runApp(MyApp());
-}
+class ActionSheetApp extends StatelessWidget {
+  const ActionSheetApp({super.key});
 
-class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Text('Flutter Demo Home Page'),
-      // ignore: prefer_const_constructors
+    return const CupertinoApp(
+      theme: CupertinoThemeData(brightness: Brightness.light),
+      home: ActionSheetExample(),
+    );
+  }
+}
+
+class ActionSheetExample extends StatelessWidget {
+  const ActionSheetExample({super.key});
+
+  // This shows a CupertinoModalPopup which hosts a CupertinoActionSheet.
+  void _showActionSheet(BuildContext context) {
+    showCupertinoModalPopup<void>(
+      context: context,
+      builder: (BuildContext context) => CupertinoActionSheet(
+        title: const Text('Title'),
+        message: const Text('Message'),
+        actions: <CupertinoActionSheetAction>[
+          CupertinoActionSheetAction(
+            /// This parameter indicates the action would be a default
+            /// defualt behavior, turns the action's text to bold text.
+            isDefaultAction: true,
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text('Default Action'),
+          ),
+          CupertinoActionSheetAction(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text('Action'),
+          ),
+          CupertinoActionSheetAction(
+            /// This parameter indicates the action would perform
+            /// a destructive action such as delete or exit and turns
+            /// the action's text color to red.
+            isDestructiveAction: true,
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text('Destructive Action'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: const CupertinoNavigationBar(
+        middle: Text('CupertinoActionSheet Sample'),
+      ),
+      child: Center(
+        child: CupertinoButton(
+          onPressed: () => _showActionSheet(context),
+          child: const Text('CupertinoActionSheet'),
+        ),
+      ),
     );
   }
 }
